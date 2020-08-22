675d6589760cdeeaef73121915669e40183a6fb2
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-22 20:34:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,9 @@
 # Write your methods here
 
-def loop_message_five_times (array)
+def loop_message_five_times (message)
+  
+
+def loop_message_n_times (array)
   for (i > array.length)
   puts array[i]
 end
